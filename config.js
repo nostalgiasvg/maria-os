3691b5cb71bea405e36cdca128d9c17374020ec9
nostalgiasvg <maria.moreno.giron@gmail.com>
2026-05-17 20:38:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// =============================================
+// MARÍA OS — Configuración
+// Sustituye los valores entre comillas
+// =============================================
+
+const WORKER_URL = 'URL_WORKER'; // https://maria-os.tusubdominio.workers.dev
+const DB_APPS    = 'ID_BBDD_1';  // ID de la base de datos Apps
+const DB_BLOG    = 'ID_BBDD_2';  // ID de la base de datos Blog Posts
+const DB_THEMES  = 'ID_BBDD_3';  // ID de la base de datos Themes
